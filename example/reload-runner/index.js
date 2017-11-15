0d77cd24ff526e0e06f62960c55ee67d40a359f8
Gabriel Weitoft (gwe) <gabriel.weitoft@qlik.com>
2017-11-15 10:36:30 +0100

    * Docs * Added check for table data not being empty in example

--- example/reload-runner/index.js
+++ example/reload-runner/index.js
@@ -82,11 +82,12 @@ session.open()
   })
   .then((tableData) => {
     if (tableData.length === 0) {
-      return Promise.reject("Empty table response")
+      return Promise.reject('Empty table response');
     }
     // Convert table grid into a string using some functional magic
     const tableDataAsString = tableData.map(row => row.qValue.map(value => value.qText).reduce((left, right) => `${left}\t${right}`)).reduce((row1, row2) => `${row1}\n${row2}`);
     console.log(tableDataAsString);
+    return undefined;
   })
   .then(() => session.close())
   .then(() => console.log('Session closed'))
